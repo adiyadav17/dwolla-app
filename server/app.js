fc5fcfde8e0cbdb0ca6cd87e239e57b305aa2547
adiyadav17 <42916853+adiyadav17@users.noreply.github.com>
2020-04-28 20:21:59 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const dwollaconfig = require('./config/dwolla');
 // Bring in the database object
 const express = require('express');
 const bodyParser = require('body-parser');
